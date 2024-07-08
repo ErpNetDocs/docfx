e5de0cd72f359e15dc28a7fcdc1b4898a90950c3
Peter Kostov <p.kostov@erp.bg>
2024-07-08 11:23:15 +0300

    changed build directory

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-cd .\templates && npm install && npm run build && cd ..\ && dotnet build -c RELEASE -v n /m:1
+cd .\templates && npm install && npm run build && cd ..\ && dotnet build -c RELEASE -v n /m:1 -o .\bin\docfx
